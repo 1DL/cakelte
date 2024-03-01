2f7c55c68c886627c1cb2a32a88a97e79559eb2c
Luiz Henrique <luizlink64@gmail.com>
2024-03-01 11:44:17 -0300

    Fixed class output

--- src/Style/SidebarHeader.php
+++ src/Style/SidebarHeader.php
@@ -11,46 +11,22 @@ class SidebarHeader implements StyleInterface
     use StyleTrait;
     private $_name = 'sidebar_header';
 
-    public const STYLE_BG_DARK_PRIMARY = 'dark_primary';
-    public const STYLE_BG_DARK_SECONDARY = 'dark_secondary';
-    public const STYLE_BG_DARK_INFO = 'dark_info';
-    public const STYLE_BG_DARK_SUCCESS = 'dark_success';
-    public const STYLE_BG_DARK_DANGER = 'dark_danger';
-    public const STYLE_BG_DARK_WARNING = 'dark_warning';
-    public const STYLE_BG_DARK_WHITE = 'dark_white';
+    public const STYLE_BG_DARK_PRIMARY      = 'navbar-primary navbar-light';
+    public const STYLE_BG_DARK_SECONDARY    = 'navbar-secondary navbar-light';
+    public const STYLE_BG_DARK_INFO         = 'navbar-info navbar-light';
+    public const STYLE_BG_DARK_SUCCESS      = 'navbar-success navbar-light';
+    public const STYLE_BG_DARK_DANGER       = 'navbar-danger navbar-light';
+    public const STYLE_BG_DARK_WARNING      = 'navbar-warning navbar-light';
+    public const STYLE_BG_DARK_WHITE        = 'navbar-white navbar-light';
 
-    public const STYLE_BG_LIGHT_PRIMARY = 'light_primary';
-    public const STYLE_BG_LIGHT_SECONDARY = 'light_secondary';
-    public const STYLE_BG_LIGHT_INFO = 'light_info';
-    public const STYLE_BG_LIGHT_SUCCESS = 'light_success';
-    public const STYLE_BG_LIGHT_DANGER = 'light_danger';
-    public const STYLE_BG_LIGHT_WARNING = 'light_warning';
-    public const STYLE_BG_LIGHT_WHITE = 'light_white';
+    public const STYLE_BG_LIGHT_PRIMARY     = 'navbar-primary navbar-dark';
+    public const STYLE_BG_LIGHT_SECONDARY   = 'navbar-secondary navbar-dark';
+    public const STYLE_BG_LIGHT_INFO        = 'navbar-info navbar-dark';
+    public const STYLE_BG_LIGHT_SUCCESS     = 'navbar-success navbar-dark';
+    public const STYLE_BG_LIGHT_DANGER      = 'navbar-danger navbar-dark';
+    public const STYLE_BG_LIGHT_WARNING     = 'navbar-warning navbar-dark';
+    public const STYLE_BG_LIGHT_WHITE       = 'navbar-white navbar-dark';
 
-    public const STYLE_BRAND_LIGHT = 'light';
-    public const STYLE_BRAND_DARK = 'dark';
-
-    private const CSS_CLASSES_BACKGROUND = [
-
-        self::STYLE_BG_DARK_PRIMARY    => 'navbar-primary navbar-light',
-        self::STYLE_BG_DARK_SECONDARY  => 'navbar-secondary navbar-light',
-        self::STYLE_BG_DARK_INFO       => 'navbar-info navbar-light',
-        self::STYLE_BG_DARK_SUCCESS    => 'navbar-success navbar-light',
-        self::STYLE_BG_DARK_DANGER     => 'navbar-danger navbar-light',
-        self::STYLE_BG_DARK_WARNING    => 'navbar-warning navbar-light',
-        self::STYLE_BG_DARK_WHITE      => 'navbar-white navbar-light',
-
-        self::STYLE_BG_LIGHT_PRIMARY    => 'navbar-primary navbar-dark',
-        self::STYLE_BG_LIGHT_SECONDARY  => 'navbar-secondary navbar-dark',
-        self::STYLE_BG_LIGHT_INFO       => 'navbar-info navbar-dark',
-        self::STYLE_BG_LIGHT_SUCCESS    => 'navbar-success navbar-dark',
-        self::STYLE_BG_LIGHT_DANGER     => 'navbar-danger navbar-dark',
-        self::STYLE_BG_LIGHT_WARNING    => 'navbar-warning navbar-dark',
-        self::STYLE_BG_LIGHT_WHITE      => 'navbar-white navbar-dark',
-    ];
-
-    private const CSS_CLASSES_BRAND = [
-        self::STYLE_BRAND_LIGHT     => 'text-light',
-        self::STYLE_BRAND_DARK      => 'text-dark',
-    ];
+    public const STYLE_BRAND_LIGHT          = 'text-light';
+    public const STYLE_BRAND_DARK           = 'text-dark';
 }
